2e2fc0b1b9687fb6049c604fe40201c9cf73ad86
hdinjos <hdinjos@gmail.com>
2024-04-22 23:04:41 +0700

    update seed product categories

---
+++ database/seeders/ProductCategorySeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+
+class ProductCategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        $cats = [
+            [
+                "name" => "laptop apik",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ],
+            [
+                "name" => "hp apik",
+                'created_at' => Carbon::now()->format('Y-m-d H:i:s'),
+                'updated_at' => Carbon::now()->format('Y-m-d H:i:s')
+            ]
+        ];
+
+        foreach ($cats as $cat) {
+            DB::table('product_categories')->insert($cat);
+        }
+    }
+}
