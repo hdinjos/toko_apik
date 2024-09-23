83d96fde8045d7c2b7f9f4ecdfa645aa27b1daf1
hdinjos <hdinjos@gmail.com>
2024-09-23 11:41:51 +0700

    change main route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return response()->json([
-        "msg" => "welcome to main page"
+        "msg" => "welcome our server"
     ]);
 });
