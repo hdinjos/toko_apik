b58ad7c61ec01d47870db8a2268f595d1fce54f3
hdinjos <hdinjos@gmail.com>
2024-09-23 16:05:49 +0700

    change message in main route

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return response()->json([
-        "msg" => "welcome our server"
+        "msg" => "welcome our server uye..."
     ]);
 });
